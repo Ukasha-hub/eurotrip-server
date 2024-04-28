ad7843dc704c451bd4d8ebe397f2f7dcc6eb975a
Ukasha-hub <ukashajljl@gmail.com>
2024-04-28 19:46:38 +0600

    touristspot POST

--- index.js
+++ index.js
@@ -29,9 +29,19 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const touristSpotCollection= client.db('touristSpotDB').collection('touristSpot');
+
+    app.post('/touristSpot', async(req,res)=>{
+      const newSpot= req.body;
+      console.log(newSpot)
+      const result =await touristSpotCollection.insertOne(newSpot)
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
